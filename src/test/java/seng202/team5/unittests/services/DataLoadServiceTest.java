235fc87ebdc897eb161675dacae7605ace29f639
Sean Reitsma <sre114@uclive.ac.nz>
2024-08-11 18:42:35 +1200

    Wrote first unit test for DataLoadService.
    Copied the first 16 lines from the winemag csv file for testing with. Added method stubs for DataLoadService to
    be completed when I have time.

---
+++ src/test/java/seng202/team5/unittests/services/DataLoadServiceTest.java
@@ -0,0 +1,34 @@
+package seng202.team5.unittests.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import seng202.team5.Wine;
+import seng202.team5.services.DataLoadService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+
+public class DataLoadServiceTest {
+  private DataLoadService dataLoadService;
+  private String csvFilePath;
+
+  @BeforeEach
+  public void setUp() {
+    csvFilePath = System.getProperty("user.dir") + "/src/test/resources/test.csv";
+    dataLoadService = new DataLoadService();
+  }
+
+
+  @Test
+  public void loadWinesTest() {
+    List<Wine> wines = dataLoadService.processWinesFromCsv(csvFilePath);
+    assertEquals(16, wines.size());
+    Wine wine = wines.getFirst();
+    assertEquals("Nicosia 2013 Vulkà Bianco  (Etna)", wine.getName());
+    assertEquals(2013, wine.getYear());
+    assertEquals(87, wine.getRating());
+  }
+}
