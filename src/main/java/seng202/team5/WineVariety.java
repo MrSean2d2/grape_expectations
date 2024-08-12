afd27a424e2cff83de3084353473bf5af064a5f3
Finn <fbr42@uclive.ac.nz>
2024-08-12 23:35:37 +1200

    Wrote WineVariety class

--- src/main/java/seng202/team5/WineVariety.java
+++ src/main/java/seng202/team5/WineVariety.java
@@ -1,4 +1,27 @@
 package seng202.team5;
 
+import seng202.team5.models.WineType;
+
+/**
+ *  Class to describe a wine's variety
+ */
 public class WineVariety {
+    String name;
+    WineType type;
+
+    /**
+     * Get the name of the wine variety
+     * @return the wine variety's name
+     */
+    String getName() {
+        return name;
+    }
+
+    /**
+     * Get the type of the wine variety
+     * @return the wine variety's type
+     */
+    WineType getType() {
+        return type;
+    }
 }
