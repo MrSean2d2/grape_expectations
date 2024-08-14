6203db95f6db2719323b2a0b6b0047bd8ede5efe
hello <mga138@uclive.ac.nz>
2024-08-15 11:36:19 +1200

    Modified docstring in Wine

--- src/main/java/seng202/team5/Wine.java
+++ src/main/java/seng202/team5/Wine.java
@@ -5,8 +5,8 @@ package seng202.team5;
  * @author Martyn Gascoigne
  */
 public class Wine {
-    private String name = null; // Default wine name
-    private String description = null; // Default description
+    private String name; // Default wine name
+    private String description; // Default description
     private int year = 0; // Bottled Year
     private int ratingValue = 0; // Rating value (from critic)
     private double price = 0.0; // Market price
@@ -14,40 +14,44 @@ public class Wine {
 
     /**
      * Gets the wine name
+     *
      * @return name
      */
     public String getName() { return name; }
 
     /**
      * Gets the wine's current market price
+     *
      * @return price
      */
     public double getPrice() { return price; }
 
     /**
      * Gets the bottled year
+     *
      * @return year
      */
     public int getYear() { return year; }
 
     /**
      * Gets the wine's rating value
+     *
      * @return ratingValue
      */
     public double getRating() { return ratingValue; }
 
     /**
      * Gets the favourite status of the wine
+     *
      * @return favourite
      */
     public boolean isFavourite() { return favourite; }
 
     /**
-     * Constructor for creating a wine object with a name 
-     * and description
+     * Constructor for creating a wine object with a name and description
      * 
-     * @param String name of wine
-     * @param String description of wine
+     * @param name name of wine
+     * @param description description of wine
      */
     public Wine(String name, String description) {
         this.name = name;
@@ -58,12 +62,12 @@ public class Wine {
      * Constructor for creating a wine object with a name,
      * description, year, rating, price and favourite status
      * 
-     * @param String name of wine
-     * @param String description of wine
-     * @param int year of wine
-     * @param int rating value of wine
-     * @param double current market price of wine
-     * @param boolean favourite status of wine
+     * @param name name of wine
+     * @param description description of wine
+     * @param year year of wine
+     * @param ratingValue rating value of wine
+     * @param price current market price of wine
+     * @param favourite favourite status of wine
      */
     public Wine(String name, String description, int year, int ratingValue, double price, boolean favourite) {
         this.name = name;
