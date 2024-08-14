3a27f2b8e7be69996090620c73207ca5bd39493f
mwi158 <mwi158@uclive.ac.nz>
2024-08-15 11:54:33 +1200

    updated wine class to have attributes from UML diagram

--- src/main/java/seng202/team5/models/Region.java
+++ src/main/java/seng202/team5/models/Region.java
@@ -7,16 +7,17 @@ import java.util.ArrayList;
  */
 public class Region {
     private String name;
-    private String description;
+    //private String description;
+    // description removed because it is not included in the csv
     private ArrayList<Region> subRegions;
     private ArrayList<Vineyard> vineyards;
 
     /**
      * Constructor
      */
-    public Region(String name, String description, ArrayList<Region> subRegions) {
+    public Region(String name, ArrayList<Region> subRegions) {
         this.name = name;
-        this.description = description;
+        //this.description = description;
         this.subRegions = subRegions;
     }
 
@@ -32,13 +33,6 @@ public class Region {
      * @return String of region name
      */
     public String getName(){return name;
-    }
-    /**
-     * Get description of region
-     * @return String of region description
-     */
-    public String getDescription(){
-        return description;
     }
 
     /**
