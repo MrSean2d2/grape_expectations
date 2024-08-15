e5468129d61d6d2bf4230a905ee07170c6d03f1a
mwi158 <mwi158@uclive.ac.nz>
2024-08-15 14:55:11 +1200

    edited wine class to save info in updated constructor

--- src/main/java/seng202/team5/models/Wine.java
+++ src/main/java/seng202/team5/models/Wine.java
@@ -33,5 +33,8 @@ public class Wine {
         this.ratingValue = ratingValue;
         this.price = price;
         //this.favourite = favourite;
+        this.wineVariety = wineVariety;
+        this.region = region;
+        this.vineyard = vineYard;
     }
 }
