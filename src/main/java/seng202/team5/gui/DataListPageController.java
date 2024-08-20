4ec6eb42adc277ff002c82b839352d902a8cfb00
Sean Reitsma <sean@doubleflatwhite.nz>
2024-08-20 14:30:10 +1200

    DataListPageController code style fixes.

--- src/main/java/seng202/team5/gui/DataListPageController.java
+++ src/main/java/seng202/team5/gui/DataListPageController.java
@@ -2,17 +2,15 @@ package seng202.team5.gui;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
-import seng202.team5.models.*;
+import seng202.team5.models.Wine;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Controller for the Data List Page.
+ */
 public class DataListPageController {
 
     @FXML
@@ -33,6 +31,10 @@ public class DataListPageController {
     @FXML
     private TableColumn<Wine, Boolean> favouriteColumn;
 
+    /**
+     * Initializes the data List by calling {@link seng202.team5.services.WineService}
+     * to populate the list of wines.
+     */
     @FXML
     public void initialize() {
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -41,35 +43,15 @@ public class DataListPageController {
         ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
         //favouriteColumn.setCellValueFactory(new PropertyValueFactory<>("favourite"));
 
-        // Populate table with test data
-
-        WineVariety dummyWine = new WineVariety("dummyVariety", WineType.RED);
-        ArrayList<Region> subRegionsList = new ArrayList<>();
-        Vineyard dummyVineyard = new Vineyard("dummyVineyard");
-        ArrayList<Vineyard> vineyardList = new ArrayList<>();
-        Region dummyRegion = new Region("dummyRegion", subRegionsList, vineyardList);
-        ObservableList<Wine> wines = FXCollections.observableArrayList(MainWindow.appEnvironment.wineService.getWineList());
-//        ObservableList<Wine> wines = FXCollections.observableArrayList(
-//                new Wine("Chardonnay", "", 2020, 4, 15.99, dummyWine, dummyRegion, dummyVineyard),
-//                new Wine("Merlot", "", 2018, 4, 10.49, dummyWine, dummyRegion, dummyVineyard),
-//                new Wine("Cabernet Sauvignon", "",  2021, 5, 20.99,dummyWine, dummyRegion, dummyVineyard)
-//        );
+        ObservableList<Wine> wines = FXCollections.observableArrayList(
+                MainWindow.appEnvironment.wineService.getWineList());
 
         // Add data to TableView
         wineTable.setItems(wines);
-        wineTable.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println(wineTable.getSelectionModel().getSelectedItem().getName());
-                if (mouseEvent.getClickCount() == 2) {
-                    System.out.println("double clicked");
-//                    MainLayoutController mainLayoutController = new MainLayoutController();
-//                    try {
-//                        mainLayoutController.loadDetailedViewPage();
-//                    } catch (Exception e) {
-//                        throw new RuntimeException(e);
-//                    }
-                }
+        wineTable.setOnMouseClicked(mouseEvent -> {
+            System.out.println(wineTable.getSelectionModel().getSelectedItem().getName());
+            if (mouseEvent.getClickCount() == 2) {
+                System.out.println("double clicked");
             }
         });
     }
