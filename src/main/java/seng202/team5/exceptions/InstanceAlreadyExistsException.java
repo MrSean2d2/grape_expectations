a7e5b33b76a127bb1f3f730761ede12327e0ec52
Sean Reitsma <sre114@uclive.ac.nz>
2024-09-10 17:13:43 +1200

    Added DatabaseService tests.
    Added some basic tests for DatabaseService to check singleton instance and
    database connection.

---
+++ src/main/java/seng202/team5/exceptions/InstanceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package seng202.team5.exceptions;
+
+public class InstanceAlreadyExistsException extends Exception {
+    public InstanceAlreadyExistsException(String message) {
+        super(message);
+    }
+}
