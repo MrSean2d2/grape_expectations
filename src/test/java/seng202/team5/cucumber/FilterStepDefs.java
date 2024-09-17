10350c92ab094d2485cf46fb25a148acda18d534
Caitlin Tam <cta108@uclive.ac.nz>
2024-09-18 02:17:23 +1200

    Filter Cucumber tests

--- src/test/java/seng202/team5/cucumber/FilterStepDefs.java
+++ src/test/java/seng202/team5/cucumber/FilterStepDefs.java
@@ -9,7 +9,12 @@ import seng202.team5.models.Wine;
 import seng202.team5.repository.VineyardDAO;
 import seng202.team5.repository.WineDAO;
 
+import java.awt.*;
 import java.util.List;
+
+/**
+ * Basic step definitions for filter feature
+ */
 
 public class FilterStepDefs {
     private WineDAO wineDAO;
@@ -33,4 +38,71 @@ public class FilterStepDefs {
             Assert.assertEquals(year, wine.getYear());
         }
     }
+
+    @When("the user applies a variety filter {string}")
+    public void theUserAppliesAVarietyFilter(String variety) {
+            String query = wineDAO.queryBuilder("0",variety,"0","0", 0.0, 800.0, 0.0, 100.0, false);
+            filteredWines = wineDAO.executeSearchFilter(query, null);
+        }
+
+    @Then("the system displays results of wine entries of the variety {string}")
+    public void theSystemDisplaysResultsOfWineEntriesOfTheVarietyChardonnay(String variety) {
+        for(Wine wine: filteredWines) {
+            Assert.assertEquals(variety, wine.getWineVariety());
+        }
+    }
+
+
+    @When("the user applies a minimum price range {int} and maximum price {int}")
+    public void theUserAppliesAMinimumPriceRangeAndMaximumPrice(int minPrice, int maxPrice) {
+        String query = wineDAO.queryBuilder("0","0","0","0", minPrice, maxPrice, 0.0, 100.0, false);
+        filteredWines = wineDAO.executeSearchFilter(query, null);
+
+    }
+
+    @Then("the system displays results of wine entries of price between {int} and {int}")
+    public void theSystemDisplaysResultsOfWineEntriesOfPriceBetweenAnd(int minPrice, int maxPrice) {
+        for(Wine wine: filteredWines) {
+            double price = wine.getPrice();
+            Assert.assertTrue(price>=minPrice && price<= maxPrice);
+        }
+    }
+
+    @When("the user applies a {int} year filter minimum rating filter of {int}.")
+    public void theUserAppliesAYearFilterMinimumRatingFilterOf(int year, int rating) {
+        String query = wineDAO.queryBuilder("0","0","0",String.valueOf(year), 0, 800.0, 0.0, rating, false);
+        filteredWines = wineDAO.executeSearchFilter(query, null);
+    }
+
+    @Then("the system displays only wines that from {int} rate {int}.")
+    public void theSystemDisplaysOnlyWinesThatFromRate(int year, int minRating) {
+        for(Wine wine: filteredWines) {
+            Assert.assertEquals(year, wine.getYear());
+            Assert.assertTrue(wine.getRating() >= minRating);
+        }
+    }
+
+    @When("the user applies filters variety {string}, region name {string}, year  {int}, min price {int}, max price {int}, min rating {int}, search bar {string}.")
+    public void theUserAppliesFiltersVarietyRegionNameYearMinPriceMaxPriceMinRatingSearchBar(String variety, String region, int year, int minPrice, int maxPrice, int minRating, String search) {
+        String query = wineDAO.queryBuilder(search,variety,region,String.valueOf(year), minPrice, maxPrice, 0, minRating, false);
+        filteredWines = wineDAO.executeSearchFilter(query, null);
+    }
+
+    @Then("the system displays results of wines with Variety {string}, Region {string}, Year {int}, Price between {int} and {int}, Rating of {int} or higher, and containing {string} in the name")
+    public void theSystemDisplaysResultsOfWinesWithVarietyRegionYearPriceBetweenAndRatingOfOrHigherAndContainingInTheName(String variety, String region, int year, int minPrice, int maxPrice, int minRating, String search) {
+        for (Wine wine:filteredWines){
+            Assert.assertEquals(wine.getWineVariety(), variety);
+            Assert.assertEquals(wine.getRegion(), region);
+            Assert.assertEquals(wine.getYear(), year);
+            Assert.assertTrue(wine.getPrice()>=minPrice && wine.getPrice()<=maxPrice);
+            Assert.assertTrue(wine.getRating()>=minRating);
+            Assert.assertTrue(wine.getName().contains(search)||wine.getDescription().contains(search));
+        }
+    }
+
+
+
+
+
 }
+
--- src/test/resources/features/Filter.feature
+++ src/test/resources/features/Filter.feature
@@ -4,3 +4,22 @@ Feature: Filter feature
     When the user applies a year 2012 filter
     Then the system displays results of wine entries from 2012.
 
+  Scenario: I want to be able to search by variety
+    Given the user is on the base search page,
+    When the user applies a variety filter "Chardonnay"
+    Then the system displays results of wine entries of the variety "Chardonnay"
+
+  Scenario: Filter wines by price range
+    Given the user is on the base search page,
+    When the user applies a minimum price range 10 and maximum price 40
+    Then the system displays results of wine entries of price between 10 and 40
+
+  Scenario: Apply year and minimum rating filters
+    Given the user is on the base search page,
+    When the user applies a 2013 year filter minimum rating filter of 94.
+    Then the system displays only wines that from 2013 rate 94.
+
+  Scenario: Apply multiple filters and a search
+    Given the user is on the base search page,
+    When the user applies filters variety "Chardonnay", region name "Central Otago", year  2006, min price 22, max price 24, min rating 83, search bar "apple".
+    Then the system displays results of wines with Variety "Chardonnay", Region "Central Otago", Year 2006, Price between 22 and 24, Rating of 83 or higher, and containing "apple" in the name
