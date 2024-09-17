d113613ad954d581cfe8c8a67463a8a21cb3a6e5
Matthew Wills <mwi158@uclive.ac.nz>
2024-09-17 14:11:47 +1200

    readded one test

--- src/test/java/seng202/team5/unittests/services/DataLoadServiceTest.java
+++ src/test/java/seng202/team5/unittests/services/DataLoadServiceTest.java
@@ -11,35 +11,41 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seng202.team5.models.Wine;
 import seng202.team5.services.DataLoadService;
+import seng202.team5.services.WineService;
 
 public class DataLoadServiceTest {
     private DataLoadService dataLoadService;
     private String csvFilePath;
+    private WineService wineservice;
 
     @BeforeEach
     public void setUp() {
+        wineservice = WineService.getInstance();
         csvFilePath = System.getProperty("user.dir") + "/src/test/resources/test.csv";
         dataLoadService = spy(new DataLoadService());
     }
 
-//    @Test
-//    public void loadFileTestFirst() {
-//        List<String[]> records = dataLoadService.loadFile(csvFilePath);
-//        String[] expectedFirst = {"0", "Italy", "Aromas include tropical fruit, broom, brimstone and dried herb. The palate isn't overly expressive, offering unripened apple, citrus and dried sage alongside brisk acidity.",
-//                new String("Vulkà Bianco".getBytes(), StandardCharsets.UTF_8), "87", null, "Sicily & Sardinia",
-//                "Etna", null, new String("Kerin O’Keefe".getBytes(), StandardCharsets.UTF_8),
-//                "@kerinokeefe", new String("Nicosia 2013 Vulkà Bianco  (Etna)".getBytes(), StandardCharsets.UTF_8),
-//                "White Blend", "Nicosia"};
-//        assertEquals(16, records.size());
-//        assertArrayEquals(expectedFirst, records.getFirst());
-//    }
+    @Test
+    public void loadFileTestFirst() {
+        List<String[]> records = dataLoadService.loadFile(csvFilePath);
+        String[] expectedFirst = {"0", "Italy", "Aromas include tropical fruit, broom, brimstone and dried herb. The palate isn't overly expressive, offering unripened apple, citrus and dried sage alongside brisk acidity.",
+                new String("Vulkà Bianco".getBytes(), StandardCharsets.UTF_8), "87", null, "Sicily & Sardinia",
+                "Etna", null, new String("Kerin O’Keefe".getBytes(), StandardCharsets.UTF_8),
+                "@kerinokeefe", new String("Nicosia 2013 Vulkà Bianco  (Etna)".getBytes(), StandardCharsets.UTF_8),
+                "White Blend", "Nicosia"};
+        assertEquals(16, records.size());
+        assertArrayEquals(expectedFirst, records.getFirst());
+    }
 
 //    @Test
 //    public void loadWinesTestFirst() {
+//        List<Wine> wines = wineservice.getWineList();
+//        System.out.println(wines.size());
+//
 //        List<Wine> wines = dataLoadService.processWinesFromCsv();
+//
 //        Wine wine = wines.getFirst();
-//        // Multiple asserts to check wine equality because Wine.equals() isn't implemented yet
-//        String expected = new String("Nicosia 2013 Vulkà Bianco  (Etna)".getBytes(), StandardCharsets.UTF_8);
+//        String expected = new String("Quinta dos Avidagos 2011 Avidagos Red (Douro)".getBytes(), StandardCharsets.UTF_8);
 //
 //        String wineName = wine.getName();
 //
