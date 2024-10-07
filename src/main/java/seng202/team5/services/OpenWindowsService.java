3468cb7c853534716803558a6922ed90f1163c99
Caitlin Tam <cta108@uclive.ac.nz>
2024-10-07 14:15:07 +1300

    fix checkstyle

--- src/main/java/seng202/team5/services/OpenWindowsService.java
+++ src/main/java/seng202/team5/services/OpenWindowsService.java
@@ -3,8 +3,6 @@ package seng202.team5.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import seng202.team5.gui.ClosableWindow;
 
 /**
