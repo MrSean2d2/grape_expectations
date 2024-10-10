d0afead3df54858bfc19c6814b62450f3bdb4d81
Amiele <anm76@uclive.ac.nz>
2024-10-10 15:09:32 +1300

    Edited and added two new cucumber tests for incorrect username, and correct credentials

--- src/test/java/seng202/team5/cucumber/LogInStepDefs.java
+++ src/test/java/seng202/team5/cucumber/LogInStepDefs.java
@@ -13,31 +13,47 @@ import seng202.team5.services.UserService;
 public class LogInStepDefs {
     private UserService userService;
     private String errorMessage;
+    private User loggedInUser;
     @Given("the user {string} with password {string} already has an account")
     public void userPasswordAccount(String username, String password) {
-        // Write code here that turns the phrase above into concrete actions
         userService = UserService.getInstance();
         userService.registerUser(username, password);
     }
     @Given("is not yet logged in")
     public void userLoggedOut() {
-        // Write code here that turns the phrase above into concrete actions
         userService.signOut();
     }
-    @When("the user enters correct username {string},  incorrect password {string}")
+    @When("username {string} with incorrect password {string} is used to log in")
     public void correctUsernameIncorrectPassword(String username, String password) throws NotFoundException, PasswordIncorrectException {
-        // Write code here that turns the phrase above into concrete actions
         try {
             userService.signinUser(username, password);
         } catch (PasswordIncorrectException e) {
             errorMessage = e.getMessage();
+        }
+    }
+    @Then("error message tells the user password is correct")
+    public void passwordIncorrect() {
+        Assertions.assertEquals("Password incorrect", errorMessage);
+
+    }
+    @When("username {string} with password {string} is used to log in")
+    public void usernamePasswordLogIn(String username, String password) throws NotFoundException, PasswordIncorrectException {
+        loggedInUser = userService.signinUser(username, password);
+    }
+    @Then("the user is logged in and taken to account page")
+    public void LoggedIn() {
+        Assertions.assertEquals("tester", loggedInUser.getUsername());
+    }
+    @When("incorrect username {string} with password {string} is used to log in")
+    public void incorrectUsernameCorrectPassword(String username, String password) throws NotFoundException, PasswordIncorrectException {
+        try {
+            userService.signinUser(username, password);
         } catch (NotFoundException e) {
             errorMessage = e.getMessage();
         }
     }
-    @Then("error message tells the user password is correct")
-    public void errorMessage() {
-        Assertions.assertEquals("Password incorrect", errorMessage);
-
+    @Then("error message tells the user username is wrong")
+    public void usernameIncorrect() {
+        Assertions.assertEquals("No user with test found", errorMessage);
     }
 }
