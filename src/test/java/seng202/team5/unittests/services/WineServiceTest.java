294d921ae02d7e88edee6601b37f114f809643c5
Sean Reitsma <sre114@uclive.ac.nz>
2024-10-12 00:29:09 +1300

    Wrote some WineService unit tests.

--- src/test/java/seng202/team5/unittests/services/WineServiceTest.java
+++ src/test/java/seng202/team5/unittests/services/WineServiceTest.java
@@ -4,22 +4,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import seng202.team5.exceptions.InstanceAlreadyExistsException;
 import seng202.team5.models.Vineyard;
 import seng202.team5.models.Wine;
+import seng202.team5.repository.VineyardDAO;
+import seng202.team5.repository.WineDAO;
+import seng202.team5.services.DataLoadService;
+import seng202.team5.services.DatabaseService;
 import seng202.team5.services.WineService;
 
 public class WineServiceTest {
     private static WineService wineService;
+    private static DatabaseService databaseService;
 
     /**
      * Initial setup.
      */
     @BeforeAll
-    public static void setUp() {
+    public static void setUp() throws InstanceAlreadyExistsException {
+        DatabaseService.removeInstance();
+        databaseService =  DatabaseService.initialiseInstanceWithUrl(
+                "jdbc:sqlite:./src/test/resources/test.db");
         wineService = WineService.getInstance();
+    }
+
+    @BeforeEach
+    public void resetDb() {
+        databaseService.resetDb();
     }
 
     /**
@@ -147,21 +165,89 @@ public class WineServiceTest {
         assertTrue(wineService.isValidWine(validWine));
     }
 
+    /**
+     * Test that Unknown gets set for blank colour.
+     */
     @Test
     public void testBlankColour() {
         String colour = "   ";
         assertEquals("Unknown", wineService.validColour(colour));
     }
 
+    /**
+     * Test that Unknown gets set for empty colour.
+     */
     @Test
     public void testEmptyColour() {
         String colour = "";
         assertEquals("Unknown", wineService.validColour(colour));
     }
 
+    /**
+     * Verify that a valid variety is indeed valid.
+     */
     @Test
     public void testValidVariety() {
         String variety = "Pinot Noir";
         assertEquals("Pinot Noir", wineService.validVariety(variety));
+    }
+
+    /**
+     * Test that getWineList returns an empty list without any wines in the database.
+     */
+    @Test
+    public void testGetWineListEmpty() {
+        WineService mockedWineService = spy(wineService);
+        doNothing().when(mockedWineService).populateDatabase(any());
+        assertEquals(0, mockedWineService.getWineList().size());
+    }
+
+    /**
+     * Test getWineList with a non-empty database.
+     */
+    @Test
+    public void testGetWineList() {
+        WineService mockedWineService = spy(wineService);
+        doNothing().when(mockedWineService).populateDatabase(any());
+        Wine wine = new Wine("Wine", "Description", 2014, 85, 10, "Pinot Noir", "Red",
+                new Vineyard("Vineyard", "Region"));
+        Wine wine1 = new Wine("Another wine", "Descriptiion", 2014, 85, 10, "Pinot Gris", "White",
+                new Vineyard("Another Vineyard", "A different region"));
+        WineDAO wineDAO = new WineDAO(new VineyardDAO());
+        wine.setId(wineDAO.add(wine));
+        wineDAO.add(wine1);
+        assertEquals(2, mockedWineService.getWineList().size());
+        assertEquals(wine, mockedWineService.getWineList().getFirst());
+    }
+
+    @Test
+    public void testDuplicateWine() {
+        Vineyard vineyard = new Vineyard("Vineyard", "Region");
+        Wine wine = new Wine("Wine", "Description", 2014, 85, 10, "Pinot Noir", "Red",
+                vineyard);
+        Wine wine1 = new Wine("Wine", "Description", 2014, 85, 10, "Pinot Noir", "Red",
+                vineyard);
+        WineDAO wineDAO = new WineDAO(new VineyardDAO());
+        wineDAO.add(wine);
+        assertTrue(wineService.checkExistingWine(wine1.getName()));
+    }
+
+    @Test
+    public void testNonDuplicateWine() {
+        Vineyard vineyard = new Vineyard("Vineyard", "Region");
+        Wine wine = new Wine("Wine", "Description", 2014, 85, 10, "Pinot Noir", "Red",
+                vineyard);
+        WineDAO wineDAO = new WineDAO(new VineyardDAO());
+        wineDAO.add(wine);
+        assertFalse(wineService.checkExistingWine("Not That Wine"));
+    }
+
+    @Test
+    public void populateDatabaseTest() {
+        DataLoadService dataLoadService = new DataLoadService(
+                System.getProperty("user.dir") + "/src/test/resources/test.csv");
+        wineService.populateDatabase(dataLoadService);
+        WineDAO wineDAO = new WineDAO(new VineyardDAO());
+        assertEquals(16, wineDAO.getAll().size());
     }
 }
